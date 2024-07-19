6808ddf8264aeb9488f0ef47794cf72ea1bb3090
Dhwanit14 <dhwanitshah16@gmail.com>
2024-07-18 23:26:33 -0400

    Add existing file

---
+++ Files/Day 8.1 - Function with input.py
@@ -0,0 +1,27 @@
+# # simple Function
+# def greet():
+#     print("Hello Angela")
+#     print("How do you do ?")
+#     print("Isn't a nice weather today?")
+
+# greet()
+
+#function the takes input = parameters
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How do you do {name}")
+
+# greet_with_name("Zeel")
+
+#functions that the more than 1 input
+# def greet_with(name,location):
+#     print(f"Hello {name}, How are you ?")
+#     print(f"How is the weather today in {location} ?")
+
+# greet_with("Dhwanit","Dholka")
+
+def greet_with(name,location):
+    print(f"Hello {name}, How are you ?")
+    print(f"How is the weather today in {location} ?")
+
+greet_with(location="Dholka",name="Dhwanit")
---
+++ Projects/Day 1 -  Band Name generator.py
@@ -0,0 +1,7 @@
+# Band Name Generator
+
+your_name = input("Whats your name: \n")
+
+city_name = input("Whats the name of you city : \n")
+
+print("Your band name is " + your_name + " " + city_name)
